5023aa0ca922c48da48967238a8042b7b2427f23
pedro josé lima <pedrlimadev@gmail.com>
2024-02-06 11:28:13 -0300

    example

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay/v2"
+)
+
+func main() {
+	say, err := cowsay.Say(
+		"Hello",
+		cowsay.Type("default"),
+		cowsay.BallonWidth(40),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(say)
+}
+
